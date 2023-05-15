cc9443c27a285e37329c1f9721fd925e6b8f33d7
Brian Farish <brian.farish@gmail.com>
2023-05-15 17:11:07 -0500

    Got a decent parse for the menu analysis table

--- parse-to-df.py
+++ parse-to-df.py
@@ -3,6 +3,8 @@
 from textractcaller.t_call import call_textract, Textract_Features
 from trp import Document
 from trp.t_pipeline import order_blocks_by_geo, convert_table_to_list
+from textractcaller.t_call import call_textract, Textract_Features, call_textract_expense
+from textractprettyprinter.t_pretty_print import convert_table_to_list
 import trp
 import json
 import boto3
@@ -20,3 +22,10 @@ for page in tdoc.pages:
         tab_list = convert_table_to_list(trp_table=table)
         print(tab_list)
         dfs.append(pd.DataFrame(tab_list))
+
+df1 = dfs[0]
+df2 = dfs[1]
+df3 = dfs[2]
+df4 = dfs[3]
+df5 = dfs[4]
+df6 = dfs[5]
